754fcea7a65cebb4096f6d949458151463e213cd
loiane <loianeg@gmail.com>
2016-09-21 19:33:49 -0300

    added nav bar + routing

---
+++ src/app/nav-bar/nav-bar.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { NavBarComponent } from './nav-bar.component';
+
+describe('Component: NavBar', () => {
+  it('should create an instance', () => {
+    let component = new NavBarComponent();
+    expect(component).toBeTruthy();
+  });
+});
