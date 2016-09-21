9ec7b8c1dceab7c806b4508976c5f737e65f70a9
loiane <loianeg@gmail.com>
2016-09-21 19:36:38 -0300

    added user and address model classes

---
+++ src/app/users/shared/user.spec.ts
@@ -0,0 +1,10 @@
+/* tslint:disable:no-unused-variable */
+
+import { addProviders, async, inject } from '@angular/core/testing';
+import {User} from './user';
+
+describe('User', () => {
+  it('should create an instance', () => {
+    expect(new User()).toBeTruthy();
+  });
+});
