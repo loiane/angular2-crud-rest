b36ed327a9101995df1c5820802334c3acdf4443
loiane <loianeg@gmail.com>
2016-09-24 12:13:14 -0300

    added email validator

---
+++ src/app/shared/basic-validators.spec.ts
@@ -0,0 +1,10 @@
+/* tslint:disable:no-unused-variable */
+
+import { addProviders, async, inject } from '@angular/core/testing';
+import {BasicValidators} from './basic-validators';
+
+describe('BasicValidators', () => {
+  it('should create an instance', () => {
+    expect(new BasicValidators()).toBeTruthy();
+  });
+});
---
+++ src/app/shared/basic-validators.ts
@@ -0,0 +1,15 @@
+import { FormControl } from '@angular/forms';
+
+export class BasicValidators {
+
+  static email (control: FormControl){
+
+    let EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return EMAIL_REGEXP.test(control.value) ? null : {
+      validateEmail: {
+        valid: false
+      }
+    };
+  }
+}
